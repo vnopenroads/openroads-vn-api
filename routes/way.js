0f10a0d272febc9b01933771ed4a9210ff76d2e6
maxgrossman <m.grossman1813@gmail.com>
2017-09-05 14:33:39 -0400

    add new vpromms~bbox route
    new route to get bounding box of a given vpromms_id

--- routes/way.js
+++ routes/way.js
@@ -6,6 +6,10 @@ var queryWays = require('../services/query-ways');
 var XML = require('../services/xml');
 var log = require('../services/log');
 var Node = require('../models/node-model');
+var bbox = require('@turf/bbox');
+var point = require('@turf/helpers').point;
+var fc = require('@turf/helpers').featureCollection;
+var Promise = require('bluebird');
 
 function serveSingleWay(req, res) {
   var wayId = parseInt(req.params.wayId || '', 10);
@@ -20,6 +24,44 @@ function serveSingleWay(req, res) {
     });
     var response = res(xmlDoc.toString());
     response.type('text/xml');
+  })
+  .catch(function (err) {
+    log.error(err);
+    return res(Boom.wrap(err));
+  });
+}
+
+// convert to decimal degree lat/lon
+function nodeCoordinates (node) {
+  return [
+    parseInt(node.longitude, 10) / 10000000,
+    parseInt(node.latitude, 10) / 10000000
+  ];
+}
+
+function singleWayBBOX(req, res) {
+  var vprommsId = req.params.vprommsId;
+  if (vprommsId.length < 10) {
+    return res(Boom.badRequest('VProMMs id must be atleast 10 digits'));
+  }
+  // get way_id for row where where the 'v' column matches vprommsId;
+  knex('current_way_tags')
+  .where('v', vprommsId)
+  .then(function (result) {
+    // get nodes of that way, then return bbox that surround those nodes
+    queryWays(knex, result[0].way_id)
+    .then(function (result) {
+      // make points from each way node, then use those points to make a bbox
+      Promise.map(result.nodes, function (node) {
+        return point(nodeCoordinates(node));
+      })
+      .then(function (points) {
+        points = fc(points);
+        const bboxRes = {};
+        bboxRes[vprommsId] = bbox(points);
+        res(bboxRes);
+      });
+    });
   })
   .catch(function (err) {
     log.error(err);
@@ -75,5 +117,10 @@ module.exports = [
     method: 'GET',
     path: '/xml/way/{wayId}',
     handler: serveSingleWay
+  },
+  {
+    method: 'GET',
+    path: '/xml/way/{vprommsId}/bbox',
+    handler: singleWayBBOX
   }
 ];
