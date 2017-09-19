aac65f3548ce227d823e55fee591dfec1c5efb9a
maxgrossman <m.grossman1813@gmail.com>
2017-09-19 12:36:45 -0400

    add admin info route and its needed services

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,9 @@
 
 var Boom = require('boom');
 var knex = require('../connection');
+var getParents = require('../services/admin').getParents;
+var formatBox = require('../services/admin').formatBOX;
+Promise = require('bluebird');
 
 // module.exports = {
   /**
@@ -108,26 +111,76 @@ module.exports = [
      * @apiSuccess {string} unitInfo.id unit id
      * @apiSuccess {array} unitInfo.parent_ids unit's parent unit id(s)
      * @apiSuccess {array} unitInfo.bbox unit's bounding box
-     * 
+     *
      *
      * @apiSuccessExample {JSON} Example Usage:
      *  curl http://localhost:4000/api/admins/commune
      *
      * @apiSuccessExample {JSON} Success-Response
-     * {
-     *  level: 'province',
-     *  'units': [
-     *    {
-     *      id: 106,
-     *      name_en: 'Bac Ninh
-     *    }
-     *    ...
-     *  ]
-     * }
+     * []
+     *
     */
     method: 'GET',
     path: '/admin/{unit_id}/info',
-    handler: function (req, res) {}
+    handler: function (req, res) {
+      // get unitId from request params
+      const unitId = req.params.unit_id;
+      // check if unitId is valid length
+      const validIdCheck = [7, 5, 3].find(idLenght => idLenght === unitId.toString().length);     
+      // serve bad request if unitId string is not an integer
+      if(!Number.isInteger(Number(unitId))) {
+        return res(Boom.badRequest('unit_id must be an integer'));
+      // serve a bad request if unitId is not of valid length
+      } else if (!validIdCheck) {
+        return res(Boom.badRequest('unit_id must be a numeric code of length 7, 5, or 3. See documentation'));
+      }
+      // make 2 queries and combine results
+      Promise.all([
+        // mirrors:
+        // SELECT type, id, parent_id, bbox FROM admin_boundaries WHERE id=${unitId}
+        knex('admin_boundaries')
+        .where({ id: unitId })
+        .select(knex.raw(`type, id, parent_id, ST_Extent(geom)`))
+        .groupBy('id')
+        .then((info) => {
+          // format response so it includes a valid bbox array and parent_ids array
+          info = info[0];
+          info.bbox = formatBox(info.st_extent);
+          info.parent_ids = getParents(info.id.toString());
+          // remove unwanted parent_id and st_extent properties
+          delete info.parent_id;
+          delete info.st_extent;
+          return info;
+        })
+        .catch((e) => {
+          console.log(e);
+          throw e;
+        }),
+        // mirrorw:
+        // SELECT id WHERE id::text LIKE '${unitid}%';
+        // ... this statement selects all ids where leading numbers in id match ${unitId}
+        knex('admin_boundaries')
+        .where(knex.raw(`id::text LIKE '${unitId}%';`))
+        .select('id')
+        .then((ids) => {
+          // return array of ids without ${unitId} (as it too is selected), then sort results
+          // in ascending order. ascending order makes sure children come before grandchildren ids
+          return ids.map(id => id[Object.keys(id)[0]])
+          .filter(id => id.toString() !== unitId)
+          .sort((a, b) => a - b );
+        })
+        .catch((e) => {
+          console.log(e);
+          throw e;
+        })
+      ])
+      .then((results) => {
+        // serve object from first query with the second query attached as a child_ids property
+        const info = results[0];
+        info.child_ids = results[1];
+        res(info);
+      });
+    }
   }
 ];
 
