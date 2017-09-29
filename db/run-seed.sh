4bf06772ab66f32cb6b431ea1645443faea495f8
maxgrossman <m.grossman1813@gmail.com>
2017-09-29 10:56:23 -0400

    move fixture dtat to s3

--- db/run-seed.sh
+++ db/run-seed.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-  COPY users FROM '/init/users.csv' DELIMITER ',' CSV HEADER;
-  COPY changesets FROM '/init/changesets.csv' DELIMITER ',' CSV HEADER;
-  COPY current_nodes FROM '/init/current_nodes.csv' DELIMITER ',' CSV HEADER;
-  COPY current_ways FROM '/init/current_ways.csv' DELIMITER ',' CSV HEADER;
-  COPY current_way_nodes FROM '/init/current_way_nodes.csv' DELIMITER ',' CSV HEADER;
-  COPY current_way_tags FROM '/init/current_way_tags.csv' DELIMITER ',' CSV HEADER;
-  COPY admin_boundaries FROM '/init/admin_boudaries.csv' DELIMITER ',' CSV HEADER;
-EOSQL
+# cp data from s3 bucket into the data folder
+aws s3 cp s3://openroads-vn-fixture/ ./db/data/ --recursive
