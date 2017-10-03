f7a5eb8edfd83f8bc5e5a5cae737811d9a4f3ef2
maxgrossman <m.grossman1813@gmail.com>
2017-10-03 18:19:51 -0400

    simplify to only include admin boundaries

--- db/sql/populate-db.sql
+++ db/sql/populate-db.sql
@@ -1,7 +1,1 @@
-COPY users FROM '/init/users.csv' DELIMITER ',' CSV HEADER;
-COPY changesets FROM '/init/changesets.csv' DELIMITER ',' CSV HEADER;
-COPY current_nodes FROM '/init/current_nodes.csv' DELIMITER ',' CSV HEADER;
-COPY current_ways FROM '/init/current_ways.csv' DELIMITER ',' CSV HEADER;
-COPY current_way_nodes FROM '/init/current_way_nodes.csv' DELIMITER ',' CSV HEADER;
-COPY current_way_tags FROM '/init/current_way_tags.csv' DELIMITER ',' CSV HEADER;
 COPY admin_boundaries FROM '/init/admin_boudaries.csv' DELIMITER ',' CSV HEADER;
