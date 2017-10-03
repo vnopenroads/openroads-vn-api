5d0ec49f79c4cd0424fc806e79bdf23dec4e0a47
Derek Lieu <dl@dereklieu.com>
2017-10-03 17:44:45 -0400

    Include task table

---
+++ db/sql/201710031721_tasks.sql
@@ -0,0 +1,13 @@
+--
+-- Name: tasks; Type: TABLE; Schema: public; Owner: -; Tablespace:
+--
+
+CREATE TABLE tasks (
+	id text PRIMARY KEY,
+	way_id int REFERENCES current_ways,
+   collisions text[]
+	pending boolean DEFAULT FALSE
+);
+
+CREATE INDEX tasks_task_idx ON tasks USING btree (id);
+CREATE INDEX tasks_way_idx ON tasks USING btree (way_id);
