f574f4dfc68a1bab44bf4269144ab3d4a69b5c9c
maxgrossman <m.grossman1813@gmail.com>
2017-10-03 16:48:42 -0400

    simplify fixture docker-compose

--- db/run-seed.sh
+++ db/run-seed.sh
@@ -2,6 +2,6 @@
 set -e
 cd /init/
 # download fixture zip file, unzip its contents, then remove the zip
-wget -qO- -O /init/fixture.zip https://s3.amazonaws.com/openroads-vn-fixture/${SEED}.zip \
+wget -qO- -O /init/fixture.zip https://s3.amazonaws.com/openroads-vn-fixture/fixture.zip \
 && unzip /init/fixture.zip \
 && rm /init/fixture.zip
