267e05002d2d074bcc456edbacc76ce2b460eec4
Derek Lieu <dl@dereklieu.com>
2017-10-04 15:26:56 -0400

    Include routes to serve task UI

---
+++ routes/tasks.js
@@ -0,0 +1,149 @@
+'use strict';
+
+var Boom = require('boom');
+const knex = require('../connection');
+const queryWays = require('../services/query-ways');
+const toGeoJSON = require('../services/osm-data-to-geojson');
+
+const properties = ['way_id', 'neighbors'];
+
+async function getNextTask (req, res) {
+  const task = await knex.select(properties)
+  .from('tasks')
+  .where('pending', false)
+  .limit(1);
+  if (!task.length) return res.sendStatus(404);
+  const ids = [task[0].way_id].concat(task[0].neighbors);
+  queryWays(knex, ids).then(function (ways) {
+    return res(toGeoJSON(ways)).type('application/json');
+  }).catch(function () {
+    return res(Boom.badImplementation('Could not retrieve task'));
+  });
+}
+
+async function getTask (req, res) {
+  const task = await knex.select(properties)
+  .from('tasks')
+  .where('id', req.params.taskId)
+  if (!task.length) return res.sendStatus(404);
+  const ids = [task[0].way_id].concat(task[0].neighbors);
+  queryWays(knex, ids).then(function (ways) {
+    return res(toGeoJSON(ways)).type('application/json');
+  }).catch(function () {
+    return res(Boom.badImplementation('Could not retrieve task'));
+  });
+}
+
+async function setTaskPending (req, res) {
+  knex('tasks').where('id', req.params.taskId).update({pending: true})
+  .then(function () {
+    return res(req.params.taskId);
+  }).catch(function () {
+    return res(Boom.badImplementation('Could not update task'));
+  });
+}
+
+module.exports = [
+  {
+    /**
+     * @api {get} /tasks/next GeoJSON - Get the next available task
+     * @apiGroup Tasks
+     * @apiName GetNextTask
+     * @apiVersion 0.3.0
+     *
+     * @apiSuccess {GeoJSON} FeatureCollection List of Roads that require merging and/or joining.
+     *
+     * @apiExample {curl} Example Usage:
+     *    curl http://localhost:4000/tasks/next
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *  {
+     *    "type": "FeatureCollection",
+     *    "properties": {},
+     *    "features": [
+     *      {
+     *        "type": "Feature",
+     *        "properties": {
+     *          "highway": "secondary",
+     *          "or_vpromms_id": "213TT00008"
+     *        },
+     *        "meta": {
+     *          "id": "87",
+     *          "changeset": "1",
+     *          "timestamp": "2017-10-04T21:48:20.702Z",
+     *          "version": "1"
+     *        },
+     *        "geometry": {
+     *          "type": "LineString",
+     *          "coordinates": [[123.8149137,9.5920337],
+     *            ...
+     *          ]}
+     *      },
+     *    ...]
+     *  }
+     */
+    method: 'GET',
+    path: '/tasks/next',
+    handler: getNextTask
+  },
+
+  {
+    /**
+     * @api {get} /tasks/:id GeoJSON - Get a specific task
+     * @apiGroup Tasks
+     * @apiName GetTask
+     * @apiVersion 0.3.0
+     *
+     * @apiSuccess {GeoJSON} FeatureCollection List of Roads that require merging and/or joining.
+     *
+     * @apiExample {curl} Example Usage:
+     *    curl http://localhost:4000/tasks/1
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *  {
+     *    "type": "FeatureCollection",
+     *    "properties": {},
+     *    "features": [
+     *      {
+     *        "type": "Feature",
+     *        "properties": {
+     *          "highway": "secondary",
+     *          "or_vpromms_id": "213TT00008"
+     *        },
+     *        "meta": {
+     *          "id": "87",
+     *          "changeset": "1",
+     *          "timestamp": "2017-10-04T21:48:20.702Z",
+     *          "version": "1"
+     *        },
+     *        "geometry": {
+     *          "type": "LineString",
+     *          "coordinates": [[123.8149137,9.5920337],
+     *            ...
+     *          ]}
+     *      },
+     *    ...]
+     *  }
+     */
+    method: 'GET',
+    path: '/tasks/{taskId}',
+    handler: getTask
+  },
+
+  {
+    /**
+     * @api {PUT} /tasks/:id Update a task's status to 'pending'
+     * @apiGroup Tasks
+     * @apiName UpdateTask
+     * @apiVersion 0.3.0
+     * @apiDescription Set a task status to pending.
+     * Returns the updated task ID.
+     *
+     * @apiExample {curl} Example Usage:
+     *  curl -X PUT http://localhost:4000/tasks/1
+     */
+    method: 'PUT',
+    path: '/tasks/{taskId}/pending',
+    handler: setTaskPending
+  }
+];
