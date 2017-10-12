1c938840b9605921b7a517036ba166a1ebdd45f2
maxgrossman <m.grossman1813@gmail.com>
2017-10-12 15:20:31 -0400

    remove newData lines

--- db/run-seed.sh
+++ db/run-seed.sh
@@ -5,5 +5,4 @@ cd /init/
 wget -qO- -O /init/fixture.zip https://s3.amazonaws.com/openroads-vn-fixture/fixture.zip \
 && unzip /init/fixture.zip \
 && rm /init/fixture.zip
-cp ./newData/* ./
-rm -rf newData
+
