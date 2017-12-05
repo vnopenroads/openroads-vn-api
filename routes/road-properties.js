2a126d348cc5006643203b5092c33eb3e38a4555
James Conkling <james.lane.conkling@gmail.com>
2017-12-05 17:46:44 -0500

    remove delete roads route

--- routes/road-properties.js
+++ routes/road-properties.js
@@ -25,31 +25,6 @@ function getIdsHandler(req, res) {
   .then(res);
 }
 
-function deleteHandler (req, res) {
-  // This method will have issues if commas are used in road IDs
-  const idsToDelete = req.query.ids && req.query.ids.split(',');
-  if (!idsToDelete) {
-    return res(Boom.badRequest('Must provide a querystring list of road `ids` to delete'));
-  }
-  if (new Set(idsToDelete).size < idsToDelete.length) {
-    return res(Boom.badRequest('Cannot have duplicate road `ids`'));
-  }
-
-  knex('road_properties')
-    .whereIn('id', idsToDelete)
-    .select('id')
-    .then(idsInDB => {
-      if (idsInDB.length < idsToDelete.length) {
-        return res(Boom.badRequest(`Some requested road IDs not present in database: ${JSON.stringify(_.difference(idsToDelete, idsInDB))}`));
-      } else {
-        knex('road_properties')
-          .whereIn('id', idsToDelete)
-          .delete()
-          .then(() => res(idsToDelete));
-      }
-    });
-}
-
 module.exports = [
   /**
    * @api {get} /properties/roads Properties by road ID
@@ -94,25 +69,5 @@ module.exports = [
     method: 'GET',
     path: '/properties/roads/ids',
     handler: getIdsHandler
-  },
-  /**
-   * @api {DELETE} /properties/roads Delete road IDs
-   * @apiVersion 0.3.0
-   * @apiGroup Properties
-   *
-   * @apiParam {Object} properties CSV of properties by road ID
-   *
-   * @apiSuccess {Array} deleted Array of IDs successfully deleted
-   *
-   * @apiExample {curl} Example Usage:
-   *  curl -X DELETE http://localhost:4000/properties/roads?ids=123AB87654,999XX01010
-   *
-   * @apiSuccessExample {json} Success-Response:
-   *  ["123AB87654", "999XX01010"]
-   */
-  {
-    method: 'DELETE',
-    path: '/properties/roads',
-    handler: deleteHandler
   }
 ];
