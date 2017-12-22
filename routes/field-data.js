b7fa68e362f49fc857b804e7fa0ba09698fc895b
James Conkling <james.lane.conkling@gmail.com>
2017-12-22 16:52:00 -0500

    correctly count roads in osm on /admin/roads/total route

--- routes/field-data.js
+++ routes/field-data.js
@@ -28,55 +28,6 @@ module.exports = [
       .whereNotNull('road_id')
       .then(roads => res(roads.map(road => road.id)));
     }
-  },
-  {
-    /**
-     * @api {get} /field/roads/total
-     * @apiGroup Field
-     * @apiName List Field Data road counts at province and district level
-     * @apiDescription Returns list of field data road counts for a specific admin level
-     * @apiVersion 0.1.0
-     *
-     * @apiParam {string} level admin level
-     *
-     * @apiSuccess {array} array of objects with vpromms and field road count
-     *
-     * @apiSuccessExample {JSON} Example Usage:
-     *  curl http://localhost:4000/field/roads/total?level=district
-     *
-     * @apiSuccessExample {JSON} Success-Response
-     * [{
-     *    "total_roads": "137",
-     *    "admin": "21YD"
-     *  },
-     *  {
-     *    "total_roads": "28",
-     *    "admin": "21TH"
-     *  },
-     *  {
-     *    "total_roads": "127",
-     *    "admin": "21TT"
-     *  },
-     *  {
-     *    "total_roads": "39",
-     *    "admin": "02BX"
-     *  },
-     *  ...
-     * ]
-     *
-    */
-    method: 'GET',
-    path: '/field/roads/total',
-    handler: function (req, res) {
-      const districtQuery = req.query.level === 'district' ? ', SUBSTRING(road_id, 4, 2)' : '';
-      knex.raw(`
-        SELECT COUNT(DISTINCT road_id) as total_roads, CONCAT(SUBSTRING(road_id, 0, 3)${districtQuery}) as admin
-        FROM field_data_geometries
-        WHERE (CONCAT(SUBSTRING(road_id, 0, 3)${districtQuery}) <> '')
-        GROUP BY admin;
-      `)
-      .then(adminRoadNum => res(adminRoadNum.rows));
-    }
   }
 ];
 
