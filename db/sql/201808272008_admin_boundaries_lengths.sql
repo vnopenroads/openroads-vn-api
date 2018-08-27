2a4a00b6c51dbcbaa1c337e36b98b825e96e5ef5
Sajjad Anwar <sajjadkm@gmail.com>
2018-08-27 20:32:35 +0530

    migration for adding new length columns

---
+++ db/sql/201808272008_admin_boundaries_lengths.sql
@@ -0,0 +1,6 @@
+--
+-- Name: admin_boundaries; Type: COLUMN; Schema: public; Owner: -
+--
+
+ALTER TABLE admin_boundaries ADD COLUMN total_length double precision;
+ALTER TABLE admin_boundaries ADD COLUMN vpromm_length double precision;
