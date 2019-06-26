93f1b30835019a8f929c80b973fba1bf8959455d
ngdnghia28 <ngd.nghia28@gmail.com>
2019-06-26 16:45:31 +0700

    update fixture data link

--- db/run-seed.sh
+++ db/run-seed.sh
@@ -2,7 +2,7 @@
 set -e
 cd /init/
 # download fixture zip file, unzip its contents, then remove the zip
-wget -qO- -O /init/fixture.zip https://s3.amazonaws.com/openroads-vn-fixture/fixture.zip \
+wget -qO- -O /init/fixture.zip https://openreadvndev-public.s3.us-east-2.amazonaws.com/fixture.zip \
 && unzip /init/fixture.zip \
 && rm /init/fixture.zip
 
